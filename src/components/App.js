4725b999502599d74c253686fab4c94089819f9c
Zayedur Rahman <zayedur.rahman@jhorotek.com>
2021-11-22 17:34:09 +0600

    login pending

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import "../styles/App.css";
 import Layout from "./Layout";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <Layout>
-      <Home />
+      {/* <Home /> */}
+      <Signup />
     </Layout>
   );
 }
